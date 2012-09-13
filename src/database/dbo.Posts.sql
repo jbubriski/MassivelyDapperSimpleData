011d30cf23b301eb033181b2be0ce161ae2a45f2
John Bubriski <bubriski@gmail.com>
2012-09-13 07:53:34 -0400

    Adds basic scripts for database

---
+++ src/database/dbo.Posts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Posts] (
+    [Id]           INT            NOT NULL IDENTITY(1, 1),
+    [DateCreated]  DATETIME       NOT NULL,
+    [DateModified] DATETIME       NOT NULL,
+    [Title]        NVARCHAR (200) NOT NULL,
+    [Content]      NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ src/database/dbo.Tags.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Tags]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(100) NOT NULL
+)
