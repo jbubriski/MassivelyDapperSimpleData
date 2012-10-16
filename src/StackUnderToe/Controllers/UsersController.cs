da5b22d3d9509bd3e19e0d8afd605def2bf1c71d
John Bubriski <bubriski@gmail.com>
2012-10-15 22:50:14 -0400

    Updates UserController Details, adds Create

--- src/StackUnderToe/Controllers/UsersController.cs
+++ src/StackUnderToe/Controllers/UsersController.cs
@@ -23,21 +23,9 @@ ORDER BY CreationDate DESC", new { Top = 20 });
             }
         }
 
-        public ActionResult Top()
-        {
-            using (var connection = ConnectionHelper.GetConnection())
-            {
-                var users = connection.Query<User>(@"
-SELECT TOP (@Top) *
-FROM Users
-ORDER BY Reputation DESC", new { Top = 20 });
-
-                return View(users);
-            }
-        }
-
         public ActionResult Oldest()
         {
+            // TODO: showcase code snippets
             using (var connection = ConnectionHelper.GetConnection())
             {
                 var users = connection.Query<User>(@"
@@ -49,31 +37,95 @@ ORDER BY CreationDate ASC", new { Top = 20 });
             }
         }
 
+        public ActionResult Top()
+        {
+            // TODO: Write this
+            using (var connection = ConnectionHelper.GetConnection())
+            {
+                var users = connection.Query<User>(@"
+SELECT TOP (@Top) *
+FROM Users
+ORDER BY Reputation DESC", new { Top = 20 });
+
+                return View(users);
+            }
+        }
+
         public ActionResult Details(int id)
         {
             var sql = @"
-SELECT TOP 1 *
-FROM Users
-WHERE Id = @Id
+SELECT *
+FROM Users u
+Join Badges b ON b.UserId = u.Id
+WHERE u.Id = @Id
 
 SELECT TOP 5 *
 FROM Posts
 WHERE PostTypeId = 1
   AND OwnerUserId = @Id
+ORDER BY CreationDate DESC
 
 SELECT TOP 5 *
 FROM Posts
 WHERE PostTypeId = 2
-  AND OwnerUserId = @Id";
+  AND OwnerUserId = @Id
+ORDER BY CreationDate DESC";
 
             using (var connection = ConnectionHelper.GetConnection())
             using (var queryResults = connection.QueryMultiple(sql, new { Id = id }))
             {
-                ViewData.Model = queryResults.Read<User>().Single();
+                UserWithBadges userWithBadges = null;
+
+                // Read<Parent, Child, Parent>(MapFunction)
+                queryResults.Read<UserWithBadges, Badge, UserWithBadges>((user, badge) =>
+                {
+                    if (userWithBadges == null)
+                    {
+                        userWithBadges = user;
+                    }
+
+                    if (!userWithBadges.Badges.ContainsKey(badge.Name))
+                    {
+                        userWithBadges.Badges.Add(badge.Name, 0);
+                    }
+
+                    userWithBadges.Badges[badge.Name] = userWithBadges.Badges[badge.Name] + 1;
+
+                    return user;
+                }).ToList();
+
+                ViewData.Model = userWithBadges;
 
                 ViewBag.Questions = queryResults.Read<Post>().ToList();
                 ViewBag.Answers = queryResults.Read<Post>().ToList();
             };
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var connection = ConnectionHelper.GetConnection())
+                {
+                    connection.Execute(@"
+    INSERT INTO Users
+    (Reputation, CreationDate, LastAccessDate, DisplayName, WebsiteUrl, Location, Age, AboutMe, Views, UpVotes, DownVotes)
+    VALUES
+    (0, GETDATE(), GETDATE(), @DisplayName, @WebsiteUrl, @Location, @Age, @AboutMe, 0, 0, 0)
+    ", user);
+                }
+
+                return RedirectToAction("");
+            }
 
             return View();
         }
---
+++ src/StackUnderToe/Models/UserWithBadges.cs
@@ -0,0 +1,55 @@
+﻿// This class was automatically generated with love by ST4bby 9/25/2012 10:08:44 PM.
+// Read more at http://jbubriski.github.com/ST4bby/
+
+namespace StackUnderToe
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class UserWithBadges
+    {
+        [Required, Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Reputation")]
+        public Nullable<int> Reputation { get; set; }
+
+        [Display(Name = "Creation Date")]
+        public Nullable<DateTime> CreationDate { get; set; }
+
+        [Display(Name = "Display Name"), StringLength(80)]
+        public string DisplayName { get; set; }
+
+        [Display(Name = "Last Access Date")]
+        public Nullable<DateTime> LastAccessDate { get; set; }
+
+        [Display(Name = "Website Url"), StringLength(400)]
+        public string WebsiteUrl { get; set; }
+
+        [Display(Name = "Location"), StringLength(200)]
+        public string Location { get; set; }
+
+        [Display(Name = "Age")]
+        public Nullable<int> Age { get; set; }
+
+        [Display(Name = "About Me")]
+        public string AboutMe { get; set; }
+
+        [Display(Name = "Views")]
+        public Nullable<int> Views { get; set; }
+
+        [Display(Name = "Up Votes")]
+        public Nullable<int> UpVotes { get; set; }
+
+        [Display(Name = "Down Votes")]
+        public Nullable<int> DownVotes { get; set; }
+
+        public Dictionary<string, int> Badges { get; set; }
+
+        public UserWithBadges()
+        {
+            Badges = new Dictionary<string, int>();
+        }
+    }
+}
