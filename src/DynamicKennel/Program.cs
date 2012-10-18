7250e8d1a2b66568a295641165c49a79e3909ee9
Jared Barboza <jared.m.barboza@gmail.com>
2012-10-18 10:18:06 -0400

    added dynamic examples, fixed one slide

---
+++ src/DynamicKennel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicKennel
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            dynamic exampleSimpleDynamic = new SimpleDynamic();
+
+            Console.WriteLine( exampleSimpleDynamic.super_method_is_super() );
+
+            Console.ReadLine();
+        }
+    }
+
+    public class SimpleDynamic : DynamicObject
+    {
+        public override bool TryInvokeMember( InvokeMemberBinder binder, object[] args, out object result )
+        {
+            if ( binder.Name.ToLower().StartsWith( "super" ) )
+            {
+                result = "The greatest thing in the world!";
+                return true; // tell runtime we have handled "super*" method!
+            }
+            return base.TryInvokeMember( binder, args, out result );
+        }
+    }
+}
---
+++ src/StackUnderToe/ConnectionHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kennel.Helpers
+{
+    public class ConnectionHelper
+    {
+        public static IDbConnection GetConnection()
+        {
+            var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Default"].ConnectionString);
+
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
